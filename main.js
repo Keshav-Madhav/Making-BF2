7c8ccf51fc85a786e85fdbbaca7f72a01c515462
Keshav Madhav <keshav2552003@gmail.com>
2024-10-02 23:45:32 +0530

    Added logic to main.js to call and run bf2

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ const runButton = document.getElementById('Run');
 const stopButton = document.getElementById('Stop');
 const downloadButton = document.getElementById('Download');
 const uploadButton = document.getElementById('Upload');
+
+
+const runCode = () => {
+  const code = codeArea.value;
+  if(!code) {
+    terminal.textContent = "Error: Code cannot be empty.";
+    return
+  }
+  const output = new BF2Interpreter().run(code);
+  terminal.textContent = output;
+}
+
+
+runButton.addEventListener('click', runCode);
