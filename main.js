754396e57f70d3b330d7b3f1b383f6080ec82840
Keshav Madhav <keshav2552003@gmail.com>
2024-10-02 22:00:12 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const codeArea = document.getElementById('code-area');
+const terminal = document.getElementById('terminal');
+
+const runButton = document.getElementById('Run');
+const stopButton = document.getElementById('Stop');
+const downloadButton = document.getElementById('Download');
+const uploadButton = document.getElementById('Upload');
